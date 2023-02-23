bf783e5e7b40ec62351f8777219bb8a43e449af7
Tsjinsin <308711441@qq.com>
2023-02-23 15:00:07 +0800

    update

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ---
 
-Hello Github!
+Sandbox
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 console.log('Hello Javascript!')
 
-kit.findId("myimg").onclick=function(){
-    window.open("https://bigonion.cn")
-}
+// kit.findId("myimg").onclick=function(){
+//     window.open("https://bigonion.cn")
+// }
